c3ffeb171264189a807289c903f73fd44758ff70
Robben9874 <37851177+Robben9874@users.noreply.github.com>
2018-09-27 19:57:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,55 @@
- const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
+client.on('ready', () => {
+  console.log('======================================')
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log('')
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log('=======================================')
+});
+
+
+
+
+
 client.on('message', message => {
-if (message.content === '!spam') {
+    if(message.content === '^^DailyForAll'){
+        message.channel.send('#daily')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === '^^AllCredit'){
+        message.channel.send('#credits')
+    }
+});
+
+
+client.on('message', message => {
+    if(message.content === '^^RepForRobben'){
+        message.channel.send('#rep <@371060496276783104>')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === '^^RepForBoss'){
+        message.channel.send('#rep <@443152649899212810>')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === '^^RepForAlashaq'){
+        message.channel.send('#rep <@346045919072092161>')
+    }
+});
+
+
+
+client.on('message', message => {
+if (message.content === "!spam" + "<@" + `${client.user.id}` + ">") {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
@@ -13,6 +60,6 @@ if (message.content === '!spam') {
           
         }
       }
-  });
+});
 
 client.login(process.env.BOT_TOKEN); 
